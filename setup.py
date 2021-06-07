5b8095e89df7ef57281370de70bc570be55f36e3
Borah <nibo@softwareag.com>
2021-06-07 17:18:55 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+	name = "flask_c8y_authorize",
+	version = "1.0.0",
+	description = "A library that helps to add RBAC for cumulocity in a flask based application.",
+	long_description_content_type="text/markdown",
+	author = "Nirmal Borah",
+	author_email = "nirmal.borah@softwareag.com",
+	url = "https://github.com/softwareag/flask-c8y-authorize",
+	license = "Apache Software License",
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"License :: OSI Approved :: Apache Software License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Intended Audience :: Developers"
+	],
+	packages = find_packages(),
+	python_requires= '>= 3.6',
+	install_requires = [
+		"flask",
+        "requests"
+	]
+)
